ba380e1d5af2a077243a415ec7ccb6011467a542
vivekyadv <vivek.yadav2611@gmail.com>
2021-06-08 09:27:28 +0530

    update array 3 pointers problem

--- Two Pointers/Tricks/3. array 3 pointers.py
+++ Two Pointers/Tricks/3. array 3 pointers.py
@@ -4,8 +4,16 @@
 # Return the minimum max(abs(A[i] - B[j]), abs(B[j] - C[k]), abs(C[k] - A[i]))
 
 # Example: A = [1, 4, 10]  B = [2, 15, 20]  C = [10, 12]
-# ans = 10 from A, 15 from B and 10 from C --> ans = 5
+# ans = max(10-15, 15-10, 10-10) --> ans = 5
 
+# Explanation: max(abs(A[i] - B[j]), abs(B[j] - C[k]), abs(C[k] - A[i]))
+# can be written as max(A[i], B[j], C[k]) - min(A[i], B[j], C[k]), we 
+# can varify this with any 3 integers
+
+# now we have min and max, to reduce maximal difference(max-min), either
+# increase the min element, or decrease the max element
+
+# Method 1: increase the min element
 def minimize(A, B, C):
     i = j = k = 0
     diff = 2**32-1
@@ -24,4 +32,25 @@ def minimize(A, B, C):
 A = [1, 4, 10]  
 B = [2, 15, 20]  
 C = [10, 12]
+print(minimize(A, B, C))
+
+
+# Method 2: decrease the max element
+def minimize(A, B, C):
+    i = len(A)-1
+    j = len(B)-1
+    k = len(C)-1
+    diff = 10**9
+    while i >= 0 and j >= 0 and k >= 0:
+        mini = min(A[i],B[j],C[k])
+        maxi = max(A[i],B[j],C[k])
+        diff = min(diff, maxi - mini)
+        if maxi is A[i]:
+            i -= 1
+        elif maxi is B[j]:
+            j -= 1
+        else:
+            k -= 1
+    return diff
+
 print(minimize(A, B, C))
